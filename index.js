2dc40b8f1b631a1d3a05ba388f1cdaa43e7930e2
Anneloes <anneloes9405@hotmail.com>
2020-01-17 02:23:15 +0000

    sort of completed geolocation fetcher, still needs dynamic ip retrieval in fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
+  return ip;
 });
+
+
+fetchCoordsByIP("66.207.199.230", (error, data) => {
+  if (error) {
+    console.log("Erroooor: " + error);
+  } else {
+    console.log(data)
+  }
+})
+
+// ASK how to give ip dynamically to fetchCoordsByIP
+
--- iss.js
+++ iss.js
@@ -1,23 +1,50 @@
-
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- */
 const request = require("request");
 
- const fetchMyIP = (callback) => {
+const fetchMyIP = (callback) => {
   request(`https://api.ipify.org?format=json`, (error, response, body) => {
     const ip = JSON.parse(body).ip;
-    if (ip.length === 0) {
+    if (error) {
       callback(error, null);
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
     } else {
       callback(null, ip);
+      return ip
     }
-  })
+  });
+};
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://ipvigilante.com/${ip}`, (error, response, body) => {
+  console.log(ip)
+  const json_parsed = JSON.parse(body);
+    const lat = json_parsed.data.latitude;
+    const lng = json_parsed.data.longitude;
+    const geo = {
+      "latitude" : lat,
+      "longitude" : lng
+    }
+    console.log("body: " + body)
+    console.log("jason_parsed: " + json_parsed)
+    console.log("lat: " + lat)
+    console.log("lng: " + lng)
+
+    if (error) {
+      callback(error, null);
+    } else if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching geo location. Response: ${body}`;
+      callback(Error(msg), null)
+      return;
+    } else {
+      callback(null, geo )
+    }
+  });
+
 }
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordsByIP };
+
+// https://geo.ipify.org/api/v1?apiKey=at_jvGYBfmz4n6ZcmASWGj0fRQ6qEFQZ
+
