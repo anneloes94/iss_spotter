26c750cc0c8c2a8bc4c028ba57db99d9c5533542
Anneloes <anneloes9405@hotmail.com>
2020-01-25 23:13:39 +0000

    Done, now refactoring

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 // index.js
 const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-/**
- * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
- * Input:
- *   - A callback with an error or results. 
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The fly-over times as an array (null if error):
- *     [ { risetime: <number>, duration: <number> }, ... ]
- */ 
 
 const flyoversMessage = (flyovers) => {
   for (let flyover of flyovers) {
-    
+    let date = new Date();
+    date.setUTCSeconds(flyover.risetime);
+    console.log(`Next pass at ${date} for ${flyover.duration} seconds!`);
   }
 }
 
@@ -21,9 +14,7 @@ nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   } else {
-    return flyoversMessage(passTimes)
+    flyoversMessage(passTimes)
   }
-  // success, print out the deets!
-  console.log(passTimes);
 });
 
